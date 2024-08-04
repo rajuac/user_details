d961d15888c335ae26454d7b4589dceca6c97684
rajuac <rajuacorama@gmail.com>
2024-08-04 21:33:02 +0530

    added signature on payload of webhook

--- app/jobs/webhook_jobs/user_details_notification.rb
+++ app/jobs/webhook_jobs/user_details_notification.rb
@@ -1,3 +1,4 @@
+# frozen_string_literal: true
 
 module WebhookJobs
   class UserDetailsNotification < ActiveJob::Base
--- app/services/webhook/user_notifier.rb
+++ app/services/webhook/user_notifier.rb
@@ -13,9 +13,12 @@ module Webhook
       return unless webhook.retry_count < 4
 
       begin
-        HTTParty.post(webhook_url, body: build_body)
+        HTTParty.post(webhook_url, body: payload.to_json, headers: {
+                        'Content-Type' => 'application/json',
+                        'X-Webhook-Signature' => signature
+                      })
         webhook.status = 'Success'
-      rescue StandardError
+      rescue StandardError => e 
         webhook.status = 'Failed'
         webhook.retry_count += 1
         WebhookJobs::UserDetailsNotification.perform_later(params)
@@ -26,19 +29,31 @@ module Webhook
     private
 
     def webhook_url
-      'https://webhook-test.com/ab02b15badf450f8fd60e58396bb4322'
+      secrets[:webhook][:user_details_url]
     end
 
     def user
       @user ||= User.find_by(id: params[:user_id])
     end
 
-    def build_body
+    def payload
       {
         user_details: user.as_json,
         event_type: params[:event_type],
         event_source: 'UserDetailsService'
       }
+    end
+
+    def signature
+      OpenSSL::HMAC.hexdigest('SHA256', webhook_secret, payload.to_json)
+    end
+
+    def webhook_secret
+      secrets[:webhook][:webhook_key]
+    end
+
+    def secrets
+      @secrets ||= Rails.application.secrets
     end
   end
 end
